75d4caef824f7a7f8ef82bf9d81cc22cc2869d73
Benjamin Morgan <benmorgan2@gmail.com>
2017-04-13 09:23:38 +0100

    Added honeycomb lattice builder

--- lattice_mc/init_lattice.py
+++ lattice_mc/init_lattice.py
@@ -1,5 +1,6 @@
 import numpy as np
 from lattice_mc import lattice, lattice_site
+from math import sqrt
 
 def square_lattice( a, b, spacing ):
     grid = np.array( list( range( 1, a * b + 1 ) ) ).reshape( a, b, order='F' )
@@ -15,6 +16,43 @@ def square_lattice( a, b, spacing ):
         sites.append( lattice_site.Site( int( it[0] ), r, neighbours, 0.0, 'L' ) )
         it.iternext()
     return lattice.Lattice( sites, cell_lengths = np.array( [ a, b, 0.0 ] ) * spacing )
+
+def honeycomb_lattice( a, b, spacing, alternating_sites=False ):
+    if alternating_sites:
+        site_labels = [ 'A', 'B', 'A', 'B' ]
+    else:
+        site_labels = [ 'L', 'L', 'L', 'L' ]
+    unit_cell_lengths = np.array( [ sqrt(3), 3.0, 0.0 ] ) * spacing
+    cell_lengths = unit_cell_lengths * np.array( [ a, b, 1.0 ] )
+    grid = np.array( list( range( 1, int( a * b * 4 + 1 ) ) ) ).reshape( a, b, 4, order='C' )
+    sites = []
+    for i in range( a ):
+        for j in range( b ):
+            # site 1
+            r = np.array( [ i * sqrt(3) * spacing, j * 3 * spacing, 0.0 ] )
+            neighbours = [ grid[ i, j, 1 ],
+                           np.roll( grid, +1, axis=0 )[ i, j, 1 ],
+                           np.roll( grid, +1, axis=1 )[ i, j, 3 ] ]
+            sites.append( lattice_site.Site( grid[ i, j, 0 ], r, neighbours, 0.0, site_labels[0] ) )
+            # site 2
+            r = np.array( [ i * sqrt(3) * spacing + sqrt(3)/2 * spacing, ( j * 3 + 0.5 ) * spacing, 0.0 ] )
+            neighbours = [ grid[ i, j, 0 ], 
+                           grid[ i, j, 2 ], 
+                           np.roll( grid, -1, axis=0 )[ i, j, 0 ] ]
+            sites.append( lattice_site.Site( grid[ i, j, 1 ], r, neighbours, 0.0, site_labels[1] ) )
+            # site 3
+            r = np.array( [ i * sqrt(3) * spacing + sqrt(3)/2 * spacing, ( j * 3 + 1.5 ) * spacing, 0.0 ] )
+            neighbours = [ grid[ i, j, 1 ],
+                           grid[ i, j, 3 ],
+                           np.roll( grid, -1, axis=0 )[ i, j, 3 ] ]
+            sites.append( lattice_site.Site( grid[ i, j, 2 ], r, neighbours, 0.0, site_labels[2] ) )
+            # site 4
+            r = np.array( [ i * sqrt(3) * spacing, ( j * 3 + 2 ) * spacing, 0.0 ] )
+            neighbours = [ grid[ i, j, 2 ], 
+                           np.roll( grid, +1, axis=0 )[ i, j, 2 ],
+                           np.roll( grid, -1, axis=1 )[ i, j, 0 ] ]
+            sites.append( lattice_site.Site( grid[ i, j, 3 ], r, neighbours, 0.0, site_labels[3] ) )
+    return lattice.Lattice( sites, cell_lengths=cell_lengths )
 
 def cubic_lattice( a, b, c, spacing ):
     grid = np.array( list( range( 1, a * b * c + 1 ) ) ).reshape( a, b, c, order='F' )
--- tests/test_init_lattice.py
+++ tests/test_init_lattice.py
@@ -37,6 +37,65 @@ class InitLatticeTestCase( unittest.TestCase ):
 
     @patch( 'lattice_mc.lattice_site.Site' )
     @patch( 'lattice_mc.lattice.Lattice' )
+    def test_honeycomb_lattice( self, mock_lattice, mock_site ):
+        a, b = 2, 1
+        mock_site.side_effect = range( a*b*4 )
+        mock_lattice.return_value = 'foo'
+        spacing = 1.0
+        lattice = init_lattice.honeycomb_lattice( a, b, spacing )
+        expected_site_calls = [ [ 1, np.array([ 0., 0., 0. ]),          [2, 4, 6], 0.0, 'L' ],
+                                [ 2, np.array([ 0.8660254, 0.5, 0. ]),  [1, 3, 5], 0.0, 'L' ],
+                                [ 3, np.array([ 0.8660254, 1.5, 0. ]),  [2, 4, 8], 0.0, 'L' ],
+                                [ 4, np.array([ 0., 2., 0. ]),          [1, 3, 7], 0.0, 'L' ],
+                                [ 5, np.array([ 1.73205081, 0., 0. ]),  [2, 6, 8], 0.0, 'L' ],
+                                [ 6, np.array([ 2.59807621, 0.5, 0. ]), [5, 1, 7], 0.0, 'L' ],
+                                [ 7, np.array([ 2.59807621, 1.5, 0. ]), [8, 6, 4], 0.0, 'L' ],
+                                [ 8, np.array([ 1.73205081, 2., 0. ]),  [3, 5, 7], 0.0, 'L' ] ]
+        for c, e in zip( mock_site.mock_calls, expected_site_calls ):
+            self.assertEqual( c[1][0], e[0] ) # site number
+            np.testing.assert_array_almost_equal( c[1][1], e[1] ) # site coordinates
+            self.assertEqual( c[1][2].sort(), e[2].sort() ) # neighbour lists
+            self.assertEqual( c[1][3], e[3] ) # ?
+            self.assertEqual( c[1][4], e[4] ) # site label
+        self.assertEqual( mock_lattice.mock_calls[0][1][0], list(range(2*1*4)) )
+        self.assertEqual( lattice, 'foo' )
+        np.testing.assert_array_almost_equal( mock_lattice.mock_calls[0][2]['cell_lengths'], np.array( [ 3.464102,  3., 0. ] ) )
+
+    @patch( 'lattice_mc.lattice_site.Site' )
+    @patch( 'lattice_mc.lattice.Lattice' )
+    def test_honeycomb_lattice_periodic_boundaries( self, mock_lattice, mock_site ):
+        a, b = 3, 3
+        mock_site.side_effect = range( a*b*4 )
+        mock_lattice.return_value = 'foo'
+        spacing = 1.0
+        lattice = init_lattice.honeycomb_lattice( a, b, spacing )	
+        expected_neighbours = { 1  : [ 2, 26, 12 ],
+                                12 : [ 1, 35, 11 ],
+                                35 : [ 36, 34, 12 ],
+                                26 : [ 27, 25, 1 ] }
+        for c in mock_site.mock_calls:
+            site_number = c[1][0]
+            if site_number in expected_neighbours:
+                site_neighbours = c[1][2]
+                self.assertEqual( site_neighbours.sort(), expected_neighbours[ site_number ].sort() )
+
+    @patch( 'lattice_mc.lattice_site.Site' )
+    @patch( 'lattice_mc.lattice.Lattice' )
+    def test_honeycomb_lattice_alternating_sites( self, mock_lattice, mock_site ):
+        a, b, = 3, 3
+        mock_site.side_effect = range( a*b*4 )
+        mock_lattice.return_value = 'foo'
+        spacing = 1.0
+        lattice = init_lattice.honeycomb_lattice( a, b, spacing, alternating_sites=True )
+        for c in mock_site.mock_calls:
+            site_label = c[1][4]
+            site_neighbours = c[1][2]
+            for n in site_neighbours:
+                neighbour_label = mock_site.mock_calls[n-1][1][4]
+                self.assertEqual( set( [ site_label, neighbour_label ] ), set( [ 'A', 'B' ] ) )
+  
+    @patch( 'lattice_mc.lattice_site.Site' )
+    @patch( 'lattice_mc.lattice.Lattice' )
     def test_cubic_lattice( self, mock_lattice, mock_site ):
         a, b, c, = 2, 2, 2
         spacing = 1.0
